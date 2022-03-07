bfbdb02be7ed0f46dfc1cee2144119dfd088b54e
Ben8120 <chuabenshern5@gmail.com>
2022-03-07 17:05:44 +0800

    added icons

--- mainTab.js
+++ mainTab.js
@@ -9,7 +9,27 @@ const Tab = createBottomTabNavigator();
 
 export default function MainTab() {
   return (
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Add') {
+              iconName = focused
+                ? 'add'
+                : 'add-circle-outline';
+            } else if (route.name === 'Process') {
+              iconName = focused ? 'analytics' : 'pulse-sharp';
+            } else if (route.name === 'More') {
+            iconName = focused ? 'menu' : 'reorder-three';
+            } 
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+
+
+      >
         <Tab.Screen name="Add" component={AddScreen} />
         <Tab.Screen name="Process" component={ProcessingScreen} />
         <Tab.Screen name="More" component={MoreScreen} />
